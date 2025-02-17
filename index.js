1e67c8f84b065e509d23bbac4dcd9ffe53b98d3a
Dan Glick <danglick@Dans-MacBook-Air.local>
2025-02-17 03:50:46 -0500

    changes

--- index.js
+++ index.js
@@ -1,18 +1,23 @@
 const PIXEL_SHIM = visualViewport.width / 10
-const BALL_SPEED_DIVISOR = 2
+const BALL_SPEED_DIVISOR = 5
 const GRAVITY = 10
 const MINIMUM_SPEED = 13
+const GROUND_HEIGHT = 100
 const GROUND_COLOR = "#36454F"
+const BALL_STARTING_XPOSITION = visualViewport.width / 2
+const BALL_STARTING_YPOSITION = visualViewport.height - 70
+const BALL_RADIUS = visualViewport.width / 20
+const BALL_COLOR = "orange"
 
-let canvas;
-let context;
+let canvas
+let context
 let ball = {
-  xPosition: visualViewport.width / 2,
-  yPosition: visualViewport.height - 70,
+  xPosition: BALL_STARTING_XPOSITION,
+  yPosition: BALL_STARTING_YPOSITION,
   xVelocity: 0,
   yVelocity: 0,
-  radius: visualViewport.width / 20,
-  color: "orange"
+  radius: BALL_RADIUS,
+  color: BALL_COLOR
 }
 let touchstart = {
   xPosition: 0,
@@ -32,10 +37,19 @@ function startGame() {
   loopGame()
 }
 
+function loopGame() {
+  context.clearRect(0, 0, canvas.width, canvas.height)
+  moveBall()
+  handleDeflections()
+  drawGround()
+  drawBall()
+  requestAnimationFrame(loopGame)
+}
+
 function handleTouchstart(e) {
   touchstart.xPosition = e.touches[0].clientX
   touchstart.yPosition = e.touches[0].clientY
-  if (isNearby(touchstart, ball)) {
+  if (isClose(touchstart, ball)) {
     isShooting = true
   }
 }
@@ -48,15 +62,6 @@ function handleTouchmove(e) {
   }
 }
 
-function loopGame() {
-  context.clearRect(0, 0, canvas.width, canvas.height)
-  moveBall()
-  drawGround()
-  drawBall()
-  handleDeflections()
-  requestAnimationFrame(loopGame)
-}
-
 function moveBall() {
   ball.xPosition += ball.xVelocity
   ball.yPosition += ball.yVelocity
@@ -67,7 +72,7 @@ function moveBall() {
 
 function drawGround() {
   context.beginPath()
-  context.rect(0, canvas.height - 100, canvas.width, 100)
+  context.rect(0, canvas.height - GROUND_HEIGHT, canvas.width, GROUND_HEIGHT)
   context.fillStyle = GROUND_COLOR
   context.fill()
 }
@@ -92,9 +97,9 @@ function handleDeflections() {
   }
 }
 
-function isNearby(objectA, objectB, threshold = PIXEL_SHIM) {
+function isClose(objectA, objectB, max_distance = PIXEL_SHIM) {
   return (
-    Math.abs(objectA.xPosition - objectB.xPosition) < threshold &&
-    Math.abs(objectA.yPosition - objectB.yPosition) < threshold
+    Math.abs(objectA.xPosition - objectB.xPosition) < max_distance &&
+    Math.abs(objectA.yPosition - objectB.yPosition) < max_distance
   )
 }
